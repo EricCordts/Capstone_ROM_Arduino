8895058cbbc2063bbfacaf0564fd94358981edbe
odelo <regan.col@northeastern.edu>
2022-02-06 16:33:40 -0500

    initial BLEread update

--- BLEread/BLEread.ino
+++ BLEread/BLEread.ino
@@ -1,57 +1,58 @@
-// Capstone Arduino Program 
+// Capstone Arduino Program
 
 #include <ArduinoBLE.h>
 #include <Arduino_LSM9DS1.h>
 
 // Variable Initialization
-int accelX=1;
-int accelY=1;
-int accelZ=1;
+byte accelX = 1;
+byte accelY = 1;
+byte accelZ = 1;
 float x, y, z;
+byte data[3] = { accelX, accelY, accelZ };
 
-// Instantiate BLE Service
-BLEService customService("1101");
-
-// Instantiate Service Characteristics 
-BLEUnsignedIntCharacteristic customXChar("2101", BLERead | BLENotify); // X coordinate value
-BLEUnsignedIntCharacteristic customYChar("2102", BLERead | BLENotify); // Y coordinate value
-BLEUnsignedIntCharacteristic customZChar("2103", BLERead | BLENotify); // Z coordinate value
+// Instantiate BLE Peripheral Service
+//BLEService accService("2a675dfb-a1b0-4c11-9ad1-031a84594196");
+BLEService accService("2ba18a92-0427-4579-9884-a3c8e53dad59");
+// Instantiate Service Characteristics
+BLECharacteristic customXChar("d81c825a-4849-4606-9b43-54214c5cd8cd", BLERead | BLENotify, 1); // X coordinate value
+BLECharacteristic customYChar("f86a30d0-3af0-413f-a21b-26a2ab665933", BLERead | BLENotify, 1); // Y coordinate value
+BLECharacteristic customZChar("7f15286c-ac14-47e7-ac68-26151a6a9e6f", BLERead | BLENotify, 1); // Z coordinate value
 
 void setup() {
   IMU.begin();
-  Serial.begin(9600); 
+  Serial.begin(9600);
   while (!Serial);
-  
-  // Pin Outputs 
+
+  // Pin Outputs
   pinMode(LED_BUILTIN, OUTPUT);
 
   // Check Initialization of BLE service
   if (!BLE.begin()) {
     Serial.println("BLE failed to Initiate");
-    delay(500);
     while (1);
   }
 
   // Set the Arduino Name that will show up over BLE
-  BLE.setLocalName("Arduino Accelerometer");
-  
-  // Set Services
-  BLE.setAdvertisedService(customService);
+  BLE.setDeviceName("yo mama");
+  BLE.setConnectable(true);
 
   // Add Service Characteristics
-  customService.addCharacteristic(customXChar);
-  customService.addCharacteristic(customYChar);
-  customService.addCharacteristic(customZChar);
+  accService.addCharacteristic(customXChar);
+  accService.addCharacteristic(customYChar);
+  accService.addCharacteristic(customZChar);
 
+
+  BLE.setLocalName("yo mama");
+  BLE.setAdvertisedServiceUuid(accService.uuid());
+  
   // Add Service w/ Characteristics attached to BLE program
-  BLE.addService(customService);
 
-  // Setup BLE write channel for values 
+  // Setup BLE write channel for values
   customXChar.writeValue(accelX);
   customYChar.writeValue(accelY);
   customYChar.writeValue(accelZ);
 
-  // Setup BLE adveristing 
+  // Setup BLE adveristing
   BLE.advertise();
 
   Serial.println("Bluetooth device is now active, waiting for connections...");
@@ -59,38 +60,37 @@ void setup() {
 
 
 void loop() {
-  // Define BLE Envrionement as the central device
-  BLEDevice central = BLE.central();
 
-  // TODO: Add comment 
-  if (central) {
-    
-    Serial.print("Connected to central: ");
-    Serial.println(central.address());
-    digitalWrite(LED_BUILTIN, HIGH);
-    while (central.connected()) {
-      delay(200);
-      read_Accel();
-      
-      customXChar.writeValue(accelX);
-      customYChar.writeValue(accelY);
-      customZChar.writeValue(accelZ);
-      
-      Serial.print("At Main Function");
-      Serial.println("");
-      Serial.println("X: ");
-      Serial.print(accelX);
-      Serial.print(" Y: ");
-      Serial.print(accelY);
-      Serial.print(" Z: ");
-      Serial.print(accelZ);
-      Serial.println("");
+  BLEDevice central = BLE.central();
+  bool m = true; 
+  while (central.connected()) {
+    if(m) { 
+      Serial.print("Arduino Connected to ");
+      Serial.println(central.localName());
+      m = false; 
     }
+    digitalWrite(LED_BUILTIN, HIGH);
+    read_Accel();
+    byte data[3] = { accelX, accelY, accelZ };
+    //BLE.setManufacturerData(data, 3);
+    customXChar.writeValue(data[0]);
+    customYChar.writeValue(data[1]);
+    customZChar.writeValue(data[2]);
+
+    Serial.print("At Main Function");
+    Serial.println("");
+    Serial.print("X: ");
+    Serial.print(accelX);
+    Serial.print(" Y: ");
+    Serial.print(accelY);
+    Serial.print(" Z: ");
+    Serial.print(accelZ);
+    Serial.println("");
   }
 
   digitalWrite(LED_BUILTIN, LOW);
-  Serial.print("Disconnected from central: ");
-  Serial.println(central.address());
+  Serial.println("Arduino Disconnected");
+  delay(500);
 }
 
 // Read the acceleration of the arduino from the IMU
@@ -98,8 +98,8 @@ void read_Accel() {
   // Check if IMU is available
   if (IMU.accelerationAvailable()) {
     IMU.readAcceleration(x, y, z);
-    accelX = (1+x)*100;
-    accelY = (1+y)*100;
-    accelZ = (1+z)*100;
+    accelX = (1 + x) * 100;
+    accelY = (1 + y) * 100;
+    accelZ = (1 + z) * 100;
   }
 }
